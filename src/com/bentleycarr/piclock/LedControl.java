12ab40d6e14b63e4d64f1a9966ccce59b9a7801f
Bentley Carr <me@bentleycarr.com>
2014-04-14 12:36:52 +1000

    fixed code

--- src/com/bentleycarr/piclock/LedControl.java
+++ src/com/bentleycarr/piclock/LedControl.java
@@ -1,206 +1,172 @@
 package com.bentleycarr.piclock;
+import com.bentleycarr.piclock.Main;
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
-import com.pi4j.io.gpio.GpioPin;
-import com.pi4j.io.gpio.GpioPinDigitalInput;
 import com.pi4j.io.gpio.GpioPinDigitalMultipurpose;
-import com.pi4j.io.gpio.GpioPinDigitalOutput;
-import com.pi4j.io.gpio.PinDirection;
 import com.pi4j.io.gpio.PinMode;
-import com.pi4j.io.gpio.PinPullResistance;
-import com.pi4j.io.gpio.PinState;
 import com.pi4j.io.gpio.RaspiPin;
-import com.pi4j.io.gpio.trigger.GpioCallbackTrigger;
-import com.pi4j.io.gpio.trigger.GpioPulseStateTrigger;
-import com.pi4j.io.gpio.trigger.GpioSetStateTrigger;
-import com.pi4j.io.gpio.trigger.GpioSyncStateTrigger;
-import com.pi4j.io.gpio.event.GpioPinListener;
-import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
-import com.pi4j.io.gpio.event.GpioPinEvent;
-import com.pi4j.io.gpio.event.GpioPinListenerDigital;
-import com.pi4j.io.gpio.event.PinEventType;
 
 public class LedControl {
 	
-	// Creates GPIO Controller Instance
-	public static final GpioController gpio = GpioFactory.getInstance();
-	
-	// Provisions 4 GPIO Pins for the Hour LEDs
-	public static final GpioPinDigitalMultipurpose hourPinOne = 
-			gpio.provisionDigitalMultipurposePin(RaspiPin.GPIO_00, PinMode.DIGITAL_INPUT);
-	public static final GpioPinDigitalMultipurpose hourPinTwo = 
-			gpio.provisionDigitalMultipurposePin(RaspiPin.GPIO_01, PinMode.DIGITAL_INPUT);
-	public static final GpioPinDigitalMultipurpose hourPinThree = 
-			gpio.provisionDigitalMultipurposePin(RaspiPin.GPIO_02, PinMode.DIGITAL_INPUT);
-	public static final GpioPinDigitalMultipurpose hourPinFour = 
-			gpio.provisionDigitalMultipurposePin(RaspiPin.GPIO_03, PinMode.DIGITAL_INPUT);
-
-	// Provisions 4 GPIO Pins for the Minute LEDs
-	public static final GpioPinDigitalMultipurpose minutePinOne = 
-			gpio.provisionDigitalMultipurposePin(RaspiPin.GPIO_04, PinMode.DIGITAL_INPUT);
-	public static final GpioPinDigitalMultipurpose minutePinTwo = 
-			gpio.provisionDigitalMultipurposePin(RaspiPin.GPIO_05, PinMode.DIGITAL_INPUT);
-	public static final GpioPinDigitalMultipurpose minutePinThree = 
-			gpio.provisionDigitalMultipurposePin(RaspiPin.GPIO_06, PinMode.DIGITAL_INPUT);
-	public static final GpioPinDigitalMultipurpose minutePinFour = 
-			gpio.provisionDigitalMultipurposePin(RaspiPin.GPIO_07, PinMode.DIGITAL_INPUT);
-	
-	
 	public static void controlHour (int hour) {
+		
+		System.out.println("Setting hour LED.");
+		System.out.println("hour = " + hour);
+		
 		switch (hour) {
 			case 1:
-				hourPinOne.setMode(PinMode.DIGITAL_OUTPUT);
-				hourPinOne.high();
+				Main.hourPinOne.setMode(PinMode.DIGITAL_OUTPUT);
+				Main.hourPinOne.high();
 				
-				hourPinFour.setMode(PinMode.DIGITAL_OUTPUT);
-				hourPinFour.low();
+				Main.hourPinFour.setMode(PinMode.DIGITAL_OUTPUT);
+				Main.hourPinFour.low();
 				
-				hourPinTwo.setMode(PinMode.DIGITAL_INPUT);
+				Main.hourPinTwo.setMode(PinMode.DIGITAL_INPUT);
 				
-				hourPinThree.setMode(PinMode.DIGITAL_INPUT);
+				Main.hourPinThree.setMode(PinMode.DIGITAL_INPUT);
 						
 				break;
 				
 			case 2:
-				hourPinFour.setMode(PinMode.DIGITAL_OUTPUT);
-				hourPinFour.high();
+				Main.hourPinFour.setMode(PinMode.DIGITAL_OUTPUT);
+				Main.hourPinFour.high();
 				
-				hourPinOne.setMode(PinMode.DIGITAL_OUTPUT);
-				hourPinOne.low();
+				Main.hourPinOne.setMode(PinMode.DIGITAL_OUTPUT);
+				Main.hourPinOne.low();
 				
-				hourPinTwo.setMode(PinMode.DIGITAL_INPUT);
+				Main.hourPinTwo.setMode(PinMode.DIGITAL_INPUT);
 				
-				hourPinThree.setMode(PinMode.DIGITAL_INPUT);
+				Main.hourPinThree.setMode(PinMode.DIGITAL_INPUT);
 						
 				break;
 			
 			case 3:
-				hourPinOne.setMode(PinMode.DIGITAL_OUTPUT);
-				hourPinOne.high();
+				Main.hourPinOne.setMode(PinMode.DIGITAL_OUTPUT);
+				Main.hourPinOne.high();
 				
-				hourPinTwo.setMode(PinMode.DIGITAL_OUTPUT);
-				hourPinTwo.low();
+				Main.hourPinTwo.setMode(PinMode.DIGITAL_OUTPUT);
+				Main.hourPinTwo.low();
 				
-				hourPinThree.setMode(PinMode.DIGITAL_INPUT);
+				Main.hourPinThree.setMode(PinMode.DIGITAL_INPUT);
 				
-				hourPinFour.setMode(PinMode.DIGITAL_INPUT);
+				Main.hourPinFour.setMode(PinMode.DIGITAL_INPUT);
 						
 				break;
 				
 			case 4:
-				hourPinTwo.setMode(PinMode.DIGITAL_OUTPUT);
-				hourPinTwo.high();
+				Main.hourPinTwo.setMode(PinMode.DIGITAL_OUTPUT);
+				Main.hourPinTwo.high();
 				
-				hourPinFour.setMode(PinMode.DIGITAL_OUTPUT);
-				hourPinFour.low();
+				Main.hourPinFour.setMode(PinMode.DIGITAL_OUTPUT);
+				Main.hourPinFour.low();
 				
-				hourPinOne.setMode(PinMode.DIGITAL_INPUT);
+				Main.hourPinOne.setMode(PinMode.DIGITAL_INPUT);
 				
-				hourPinThree.setMode(PinMode.DIGITAL_INPUT);
+				Main.hourPinThree.setMode(PinMode.DIGITAL_INPUT);
 						
 				break;
 				
 			case 5:
-				hourPinTwo.setMode(PinMode.DIGITAL_OUTPUT);
-				hourPinTwo.high();
+				Main.hourPinTwo.setMode(PinMode.DIGITAL_OUTPUT);
+				Main.hourPinTwo.high();
 				
-				hourPinOne.setMode(PinMode.DIGITAL_OUTPUT);
-				hourPinOne.low();
+				Main.hourPinOne.setMode(PinMode.DIGITAL_OUTPUT);
+				Main.hourPinOne.low();
 				
-				hourPinThree.setMode(PinMode.DIGITAL_INPUT);
+				Main.hourPinThree.setMode(PinMode.DIGITAL_INPUT);
 				
-				hourPinFour.setMode(PinMode.DIGITAL_INPUT);
+				Main.hourPinFour.setMode(PinMode.DIGITAL_INPUT);
 						
 				break;
 				
 			case 6:
-				hourPinFour.setMode(PinMode.DIGITAL_OUTPUT);
-				hourPinFour.high();
+				Main.hourPinFour.setMode(PinMode.DIGITAL_OUTPUT);
+				Main.hourPinFour.high();
 				
-				hourPinTwo.setMode(PinMode.DIGITAL_OUTPUT);
-				hourPinTwo.low();
+				Main.hourPinTwo.setMode(PinMode.DIGITAL_OUTPUT);
+				Main.hourPinTwo.low();
 				
-				hourPinOne.setMode(PinMode.DIGITAL_INPUT);
+				Main.hourPinOne.setMode(PinMode.DIGITAL_INPUT);
 				
-				hourPinThree.setMode(PinMode.DIGITAL_INPUT);
+				Main.hourPinThree.setMode(PinMode.DIGITAL_INPUT);
 						
 				break;
 				
 			case 7:
-				hourPinThree.setMode(PinMode.DIGITAL_OUTPUT);
-				hourPinThree.high();
+				Main.hourPinThree.setMode(PinMode.DIGITAL_OUTPUT);
+				Main.hourPinThree.high();
 				
-				hourPinTwo.setMode(PinMode.DIGITAL_OUTPUT);
-				hourPinTwo.low();
+				Main.hourPinTwo.setMode(PinMode.DIGITAL_OUTPUT);
+				Main.hourPinTwo.low();
 				
-				hourPinOne.setMode(PinMode.DIGITAL_INPUT);
+				Main.hourPinOne.setMode(PinMode.DIGITAL_INPUT);
 				
-				hourPinFour.setMode(PinMode.DIGITAL_INPUT);
+				Main.hourPinFour.setMode(PinMode.DIGITAL_INPUT);
 						
 				break;
 				
 			case 8:
-				hourPinThree.setMode(PinMode.DIGITAL_OUTPUT);
-				hourPinThree.high();
+				Main.hourPinThree.setMode(PinMode.DIGITAL_OUTPUT);
+				Main.hourPinThree.high();
 				
-				hourPinFour.setMode(PinMode.DIGITAL_OUTPUT);
-				hourPinFour.low();
+				Main.hourPinFour.setMode(PinMode.DIGITAL_OUTPUT);
+				Main.hourPinFour.low();
 				
-				hourPinOne.setMode(PinMode.DIGITAL_INPUT);
+				Main.hourPinOne.setMode(PinMode.DIGITAL_INPUT);
 				
-				hourPinTwo.setMode(PinMode.DIGITAL_INPUT);
+				Main.hourPinTwo.setMode(PinMode.DIGITAL_INPUT);
 						
 				break;
 			
 			case 9:
-				hourPinOne.setMode(PinMode.DIGITAL_OUTPUT);
-				hourPinOne.high();
+				Main.hourPinOne.setMode(PinMode.DIGITAL_OUTPUT);
+				Main.hourPinOne.high();
 				
-				hourPinTwo.setMode(PinMode.DIGITAL_OUTPUT);
-				hourPinTwo.low();
+				Main.hourPinTwo.setMode(PinMode.DIGITAL_OUTPUT);
+				Main.hourPinTwo.low();
 				
-				hourPinThree.setMode(PinMode.DIGITAL_INPUT);
+				Main.hourPinThree.setMode(PinMode.DIGITAL_INPUT);
 				
-				hourPinFour.setMode(PinMode.DIGITAL_INPUT);
+				Main.hourPinFour.setMode(PinMode.DIGITAL_INPUT);
 						
 				break;
 				
 			case 10:
-				hourPinTwo.setMode(PinMode.DIGITAL_OUTPUT);
-				hourPinTwo.high();
+				Main.hourPinTwo.setMode(PinMode.DIGITAL_OUTPUT);
+				Main.hourPinTwo.high();
 				
-				hourPinFour.setMode(PinMode.DIGITAL_OUTPUT);
-				hourPinFour.low();
+				Main.hourPinFour.setMode(PinMode.DIGITAL_OUTPUT);
+				Main.hourPinFour.low();
 				
-				hourPinOne.setMode(PinMode.DIGITAL_INPUT);
+				Main.hourPinOne.setMode(PinMode.DIGITAL_INPUT);
 				
-				hourPinThree.setMode(PinMode.DIGITAL_INPUT);
+				Main.hourPinThree.setMode(PinMode.DIGITAL_INPUT);
 						
 				break;
 				
 			case 11:
-				hourPinOne.setMode(PinMode.DIGITAL_OUTPUT);
-				hourPinOne.high();
+				Main.hourPinOne.setMode(PinMode.DIGITAL_OUTPUT);
+				Main.hourPinOne.high();
 				
-				hourPinTwo.setMode(PinMode.DIGITAL_OUTPUT);
-				hourPinTwo.low();
+				Main.hourPinTwo.setMode(PinMode.DIGITAL_OUTPUT);
+				Main.hourPinTwo.low();
 				
-				hourPinThree.setMode(PinMode.DIGITAL_INPUT);
+				Main.hourPinThree.setMode(PinMode.DIGITAL_INPUT);
 				
-				hourPinFour.setMode(PinMode.DIGITAL_INPUT);
+				Main.hourPinFour.setMode(PinMode.DIGITAL_INPUT);
 						
 				break;
 				
 			case 12:
-				hourPinTwo.setMode(PinMode.DIGITAL_OUTPUT);
-				hourPinTwo.high();
+				Main.hourPinTwo.setMode(PinMode.DIGITAL_OUTPUT);
+				Main.hourPinTwo.high();
 				
-				hourPinOne.setMode(PinMode.DIGITAL_OUTPUT);
-				hourPinOne.low();
+				Main.hourPinOne.setMode(PinMode.DIGITAL_OUTPUT);
+				Main.hourPinOne.low();
 				
-				hourPinThree.setMode(PinMode.DIGITAL_INPUT);
+				Main.hourPinThree.setMode(PinMode.DIGITAL_INPUT);
 				
-				hourPinFour.setMode(PinMode.DIGITAL_INPUT);
+				Main.hourPinFour.setMode(PinMode.DIGITAL_INPUT);
 						
 				break;
 			
@@ -208,160 +174,164 @@ public class LedControl {
 	}
 
 	public static void controlMinute (int minute) {
+		
+		System.out.println("Setting minute LED.");
+		System.out.println("minute = " + minute);
+		
 		switch (minute) {
 			case 1:
-				minutePinOne.setMode(PinMode.DIGITAL_OUTPUT);
-				minutePinOne.high();
+				Main.minutePinOne.setMode(PinMode.DIGITAL_OUTPUT);
+				Main.minutePinOne.high();
 				
-				minutePinFour.setMode(PinMode.DIGITAL_OUTPUT);
-				minutePinFour.low();
+				Main.minutePinFour.setMode(PinMode.DIGITAL_OUTPUT);
+				Main.minutePinFour.low();
 				
-				minutePinTwo.setMode(PinMode.DIGITAL_INPUT);
+				Main.minutePinTwo.setMode(PinMode.DIGITAL_INPUT);
 				
-				minutePinThree.setMode(PinMode.DIGITAL_INPUT);
+				Main.minutePinThree.setMode(PinMode.DIGITAL_INPUT);
 						
 				break;
 				
 			case 2:
-				minutePinFour.setMode(PinMode.DIGITAL_OUTPUT);
-				minutePinFour.high();
+				Main.minutePinFour.setMode(PinMode.DIGITAL_OUTPUT);
+				Main.minutePinFour.high();
 				
-				minutePinOne.setMode(PinMode.DIGITAL_OUTPUT);
-				minutePinOne.low();
+				Main.minutePinOne.setMode(PinMode.DIGITAL_OUTPUT);
+				Main.minutePinOne.low();
 				
-				minutePinTwo.setMode(PinMode.DIGITAL_INPUT);
+				Main.minutePinTwo.setMode(PinMode.DIGITAL_INPUT);
 				
-				minutePinThree.setMode(PinMode.DIGITAL_INPUT);
+				Main.minutePinThree.setMode(PinMode.DIGITAL_INPUT);
 						
 				break;
 			
 			case 3:
-				minutePinOne.setMode(PinMode.DIGITAL_OUTPUT);
-				minutePinOne.high();
+				Main.minutePinOne.setMode(PinMode.DIGITAL_OUTPUT);
+				Main.minutePinOne.high();
 				
-				minutePinTwo.setMode(PinMode.DIGITAL_OUTPUT);
-				minutePinTwo.low();
+				Main.minutePinTwo.setMode(PinMode.DIGITAL_OUTPUT);
+				Main.minutePinTwo.low();
 				
-				minutePinThree.setMode(PinMode.DIGITAL_INPUT);
+				Main.minutePinThree.setMode(PinMode.DIGITAL_INPUT);
 				
-				minutePinFour.setMode(PinMode.DIGITAL_INPUT);
+				Main.minutePinFour.setMode(PinMode.DIGITAL_INPUT);
 						
 				break;
 				
 			case 4:
-				minutePinTwo.setMode(PinMode.DIGITAL_OUTPUT);
-				minutePinTwo.high();
+				Main.minutePinTwo.setMode(PinMode.DIGITAL_OUTPUT);
+				Main.minutePinTwo.high();
 				
-				minutePinFour.setMode(PinMode.DIGITAL_OUTPUT);
-				minutePinFour.low();
+				Main.minutePinFour.setMode(PinMode.DIGITAL_OUTPUT);
+				Main.minutePinFour.low();
 				
-				minutePinOne.setMode(PinMode.DIGITAL_INPUT);
+				Main.minutePinOne.setMode(PinMode.DIGITAL_INPUT);
 				
-				minutePinThree.setMode(PinMode.DIGITAL_INPUT);
+				Main.minutePinThree.setMode(PinMode.DIGITAL_INPUT);
 						
 				break;
 				
 			case 5:
-				minutePinTwo.setMode(PinMode.DIGITAL_OUTPUT);
-				minutePinTwo.high();
+				Main.minutePinTwo.setMode(PinMode.DIGITAL_OUTPUT);
+				Main.minutePinTwo.high();
 				
-				minutePinOne.setMode(PinMode.DIGITAL_OUTPUT);
-				minutePinOne.low();
+				Main.minutePinOne.setMode(PinMode.DIGITAL_OUTPUT);
+				Main.minutePinOne.low();
 				
-				minutePinThree.setMode(PinMode.DIGITAL_INPUT);
+				Main.minutePinThree.setMode(PinMode.DIGITAL_INPUT);
 				
-				minutePinFour.setMode(PinMode.DIGITAL_INPUT);
+				Main.minutePinFour.setMode(PinMode.DIGITAL_INPUT);
 						
 				break;
 				
 			case 6:
-				minutePinFour.setMode(PinMode.DIGITAL_OUTPUT);
-				minutePinFour.high();
+				Main.minutePinFour.setMode(PinMode.DIGITAL_OUTPUT);
+				Main.minutePinFour.high();
 				
-				minutePinTwo.setMode(PinMode.DIGITAL_OUTPUT);
-				minutePinTwo.low();
+				Main.minutePinTwo.setMode(PinMode.DIGITAL_OUTPUT);
+				Main.minutePinTwo.low();
 				
-				minutePinOne.setMode(PinMode.DIGITAL_INPUT);
+				Main.minutePinOne.setMode(PinMode.DIGITAL_INPUT);
 				
-				minutePinThree.setMode(PinMode.DIGITAL_INPUT);
+				Main.minutePinThree.setMode(PinMode.DIGITAL_INPUT);
 						
 				break;
 				
 			case 7:
-				minutePinThree.setMode(PinMode.DIGITAL_OUTPUT);
-				minutePinThree.high();
+				Main.minutePinThree.setMode(PinMode.DIGITAL_OUTPUT);
+				Main.minutePinThree.high();
 				
-				minutePinTwo.setMode(PinMode.DIGITAL_OUTPUT);
-				minutePinTwo.low();
+				Main.minutePinTwo.setMode(PinMode.DIGITAL_OUTPUT);
+				Main.minutePinTwo.low();
 				
-				minutePinOne.setMode(PinMode.DIGITAL_INPUT);
+				Main.minutePinOne.setMode(PinMode.DIGITAL_INPUT);
 				
-				minutePinFour.setMode(PinMode.DIGITAL_INPUT);
+				Main.minutePinFour.setMode(PinMode.DIGITAL_INPUT);
 						
 				break;
 				
 			case 8:
-				minutePinThree.setMode(PinMode.DIGITAL_OUTPUT);
-				minutePinThree.high();
+				Main.minutePinThree.setMode(PinMode.DIGITAL_OUTPUT);
+				Main.minutePinThree.high();
 				
-				minutePinFour.setMode(PinMode.DIGITAL_OUTPUT);
-				minutePinFour.low();
+				Main.minutePinFour.setMode(PinMode.DIGITAL_OUTPUT);
+				Main.minutePinFour.low();
 				
-				minutePinOne.setMode(PinMode.DIGITAL_INPUT);
+				Main.minutePinOne.setMode(PinMode.DIGITAL_INPUT);
 				
-				minutePinTwo.setMode(PinMode.DIGITAL_INPUT);
+				Main.minutePinTwo.setMode(PinMode.DIGITAL_INPUT);
 						
 				break;
 			
 			case 9:
-				minutePinOne.setMode(PinMode.DIGITAL_OUTPUT);
-				minutePinOne.high();
+				Main.minutePinOne.setMode(PinMode.DIGITAL_OUTPUT);
+				Main.minutePinOne.high();
 				
-				minutePinTwo.setMode(PinMode.DIGITAL_OUTPUT);
-				minutePinTwo.low();
+				Main.minutePinTwo.setMode(PinMode.DIGITAL_OUTPUT);
+				Main.minutePinTwo.low();
 				
-				minutePinThree.setMode(PinMode.DIGITAL_INPUT);
+				Main.minutePinThree.setMode(PinMode.DIGITAL_INPUT);
 				
-				minutePinFour.setMode(PinMode.DIGITAL_INPUT);
+				Main.minutePinFour.setMode(PinMode.DIGITAL_INPUT);
 						
 				break;
 				
 			case 10:
-				minutePinTwo.setMode(PinMode.DIGITAL_OUTPUT);
-				minutePinTwo.high();
+				Main.minutePinTwo.setMode(PinMode.DIGITAL_OUTPUT);
+				Main.minutePinTwo.high();
 				
-				minutePinFour.setMode(PinMode.DIGITAL_OUTPUT);
-				minutePinFour.low();
+				Main.minutePinFour.setMode(PinMode.DIGITAL_OUTPUT);
+				Main.minutePinFour.low();
 				
-				minutePinOne.setMode(PinMode.DIGITAL_INPUT);
+				Main.minutePinOne.setMode(PinMode.DIGITAL_INPUT);
 				
-				minutePinThree.setMode(PinMode.DIGITAL_INPUT);
+				Main.minutePinThree.setMode(PinMode.DIGITAL_INPUT);
 						
 				break;
 				
 			case 11:
-				minutePinOne.setMode(PinMode.DIGITAL_OUTPUT);
-				minutePinOne.high();
+				Main.minutePinOne.setMode(PinMode.DIGITAL_OUTPUT);
+				Main.minutePinOne.high();
 				
-				minutePinTwo.setMode(PinMode.DIGITAL_OUTPUT);
-				minutePinTwo.low();
+				Main.minutePinTwo.setMode(PinMode.DIGITAL_OUTPUT);
+				Main.minutePinTwo.low();
 				
-				minutePinThree.setMode(PinMode.DIGITAL_INPUT);
+				Main.minutePinThree.setMode(PinMode.DIGITAL_INPUT);
 				
-				minutePinFour.setMode(PinMode.DIGITAL_INPUT);
+				Main.minutePinFour.setMode(PinMode.DIGITAL_INPUT);
 						
 				break;
 				
 			case 12:
-				minutePinTwo.setMode(PinMode.DIGITAL_OUTPUT);
-				minutePinTwo.high();
+				Main.minutePinTwo.setMode(PinMode.DIGITAL_OUTPUT);
+				Main.minutePinTwo.high();
 				
-				minutePinOne.setMode(PinMode.DIGITAL_OUTPUT);
-				minutePinOne.low();
+				Main.minutePinOne.setMode(PinMode.DIGITAL_OUTPUT);
+				Main.minutePinOne.low();
 				
-				minutePinThree.setMode(PinMode.DIGITAL_INPUT);
+				Main.minutePinThree.setMode(PinMode.DIGITAL_INPUT);
 				
-				minutePinFour.setMode(PinMode.DIGITAL_INPUT);
+				Main.minutePinFour.setMode(PinMode.DIGITAL_INPUT);
 						
 				break;
 			
--- src/com/bentleycarr/piclock/Main.java
+++ src/com/bentleycarr/piclock/Main.java
@@ -1,5 +1,11 @@
 package com.bentleycarr.piclock;
 import com.bentleycarr.piclock.LedControl;
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalMultipurpose;
+import com.pi4j.io.gpio.PinMode;
+import com.pi4j.io.gpio.RaspiPin;
+
 import java.util.Calendar;
 
 
@@ -8,26 +14,66 @@ public class Main {
 	// Which LED should be lighted.
 	static int hourNumber;
 	static int minuteNumber;
+	
+	// Creates GPIO Controller Instance
+	static final GpioController gpio = GpioFactory.getInstance();
+	
+	// Provisions 4 GPIO Pins for the Hour LEDs
+	static final GpioPinDigitalMultipurpose hourPinOne = 
+			gpio.provisionDigitalMultipurposePin(RaspiPin.GPIO_00, PinMode.DIGITAL_INPUT);
+	static final GpioPinDigitalMultipurpose hourPinTwo = 
+			gpio.provisionDigitalMultipurposePin(RaspiPin.GPIO_01, PinMode.DIGITAL_INPUT);
+	static final GpioPinDigitalMultipurpose hourPinThree = 
+			gpio.provisionDigitalMultipurposePin(RaspiPin.GPIO_02, PinMode.DIGITAL_INPUT);
+	static final GpioPinDigitalMultipurpose hourPinFour = 
+			gpio.provisionDigitalMultipurposePin(RaspiPin.GPIO_03, PinMode.DIGITAL_INPUT);
+
+	// Provisions 4 GPIO Pins for the Minute LEDs
+	static final GpioPinDigitalMultipurpose minutePinOne = 
+			gpio.provisionDigitalMultipurposePin(RaspiPin.GPIO_04, PinMode.DIGITAL_INPUT);
+	static final GpioPinDigitalMultipurpose minutePinTwo = 
+			gpio.provisionDigitalMultipurposePin(RaspiPin.GPIO_05, PinMode.DIGITAL_INPUT);
+	static final GpioPinDigitalMultipurpose minutePinThree = 
+			gpio.provisionDigitalMultipurposePin(RaspiPin.GPIO_06, PinMode.DIGITAL_INPUT);
+	static final GpioPinDigitalMultipurpose minutePinFour = 
+			gpio.provisionDigitalMultipurposePin(RaspiPin.GPIO_07, PinMode.DIGITAL_INPUT);
 		
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
-		getMinutes();
-		getHours();
 		
-		LedControl.controlHour(hourNumber);
-		LedControl.controlMinute(minuteNumber);
+				
+		System.out.println("");
+		System.out.println("PiClock - An LED Analog Clock");
+		System.out.println("Designed for the Raspberry Pi");
+		System.out.println("By Bentley Carr");
+		System.out.println("");
 		
+		while (true) {
+			
+			getMinutes();
+			getHours();
+			
+			LedControl.controlHour(hourNumber);
+			LedControl.controlMinute(minuteNumber);
+			
+			System.out.println("Thread going to sleep for 20000 milliseconds.");
+			Thread.sleep(20000);
+			System.out.println("Thread awake.");
+			
+		}
 	}
 	
 	public static void getMinutes () {
 		
 		minuteNumber = (int)((Calendar.MINUTE + (Calendar.SECOND / 60) - 2.49)/5);
+		System.out.println("Will go to minute no. " + minuteNumber + ".");
 		
 	}
 	
 	public static void getHours () {
 		
 		hourNumber = (int)((Calendar.HOUR + ((Calendar.MINUTE + (Calendar.SECOND /60) / 60) - 2.49)) / 5);
+		System.out.println("Will go to  hour no. " + hourNumber + ".");
 		
 	}
 	
